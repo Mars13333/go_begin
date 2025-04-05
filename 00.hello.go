2b1c3f647390d266e797c00b0995aa3ad77216ac
陈天霸 <s1yuan_chen@163.com>
2025-04-05 18:30:21 +0800

    fix

--- 00.hello.go
+++ 00.hello.go
@@ -1,4 +1,5 @@
 package main // 创建文件默认的值是go_begin,改成main之后才能run
+import "fmt"
 
 func main() {
 	hello("疯狂的世界")
@@ -6,4 +7,7 @@ func main() {
 
 func hello(str string) {
 	println("hello! ", str)
+	//numbers := [16]int{1, 2, 3, 4}
+	n1 := [256]int{'a': 111, 'b': 8, 'c': 9}
+	fmt.Println(n1)
 }
