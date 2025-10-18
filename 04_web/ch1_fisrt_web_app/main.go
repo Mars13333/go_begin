37d8dff7729094c06b48ff8d3e353776d36c970b
陈天霸 <s1yuan_chen@163.com>
2025-10-18 18:27:37 +0800

    fix: last

---
+++ 04_web/ch1_fisrt_web_app/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "home",
+		})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
